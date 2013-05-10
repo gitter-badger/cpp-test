62d622f128e84fe9d6da10a7f14a3ab4bf689997
gadmyth <gadmyth@gmail.com>
2013-05-10 17:11:50 +0800

    add translate macro, 增加转换宏

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include "util.h"
 
+#define _MT(num)  CONS(macro_test_, num)
+#define MT(num) _MT(num)
+
+
 int main(char* argv) {
+	macro_test();
+	return 0;
+}
+
+void macro_test() {
+	MT(1)();
+	MT(2)();
+}
+
+void macro_test_1() {
 	printf("%s\n", STR(abc));
 	printf("%s\n", STR(CONS(abc, def)));
-	return 0;
+}
+
+void macro_test_2() {
+
 }
