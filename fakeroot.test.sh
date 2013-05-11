83f87d9ec7566c204dc7a14b07230acd9954e2b6
gadmyth <gadmyth@gmail.com>
2013-05-11 16:06:42 +0800

    fakeroot文件权限测试

---
+++ fakeroot.test.sh
@@ -0,0 +1,31 @@
+# run the test code one-by-one in shell
+# you can change the user below as yours
+TEST_USER=ibm
+
+whoami
+
+fakeroot
+
+whoami
+
+mknod hda3 b 3 1
+
+ls -ld hda3
+
+chown $TEST_USER hda3
+
+ld -ld hda3
+
+ls -ld /
+
+chown $TEST_USER /
+
+chmod 777 /
+
+ls -ld /
+
+exit
+
+ls -ld hda3
+
+ls -ld /
